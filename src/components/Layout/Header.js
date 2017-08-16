aa470d6341228486243c70586bd09ed3ebf5cba6
cloudy <1197269596@qq.com>
2017-08-16 16:40:46 +0800

    修改

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,10 +2,57 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 import styles from './Header.less'
+import { Menu, Icon, Popover } from 'antd'
+import Menus from './Menu'
+
+const SubMenu = Menu.SubMenu
 
 const Header = ({ user, logout, switchSider, siderFold, isNavbar, menuPopoverVisible, location, switchMenuPopover, navOpenKeys, changeOpenKeys, menu }) => {
+  let handleClickMenu = e => e.key === 'logout' && logout()
+
+  const menusProps = {
+    menu,
+    siderFold: false,
+    darkTheme: false,
+    isNavbar,
+    handleClickNavMenu: switchMenuPopover,
+    location,
+    navOpenKeys,
+    changeOpenKeys,
+  }
+
   return (
-    <div>this is header!</div>
+    <div className={styles.header}>
+      {isNavbar
+        ?
+        <Popover placement="bottomLeft" onVisibleChange={switchMenuPopover} visible={menuPopoverVisible} overlayClassName={styles.popvermenu} trigger="click" content={<Menus {...menusProps} />}>
+          <div className={styles.button}>
+            <Icon type="bars" />
+          </div>
+        </Popover>
+        :
+        <div className={styles.button}
+            onClick={switchSider}>
+            <Icon type={classnames({ 'menu-unfold': siderFold, 'menu-fold': !siderFold })} />
+        </div>
+      }
+      <div className={styles.rightWarpper}>
+        <div className={styles.button}>
+          <Icon type="mail" />
+        </div>
+        <Menu mode="horizontal" onClick={handleClickMenu}>
+          <SubMenu
+            style={{
+              float: 'right',
+            }}
+            title={
+              <span><Icon type="user" />{user.username}</span>
+            }>
+            <Menu.Item key="logout">Sign Out</Menu.Item>
+          </SubMenu>
+        </Menu>
+      </div>
+    </div>
   )
 }
 
--- src/components/Layout/Menu.js
+++ src/components/Layout/Menu.js
@@ -1,12 +1,86 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Menu, Icon } from 'antd'
+import { Link } from 'dva/router'
 import { arrayToTree, queryArray } from '../../utils'
 import PathToRegexp from 'path-to-regexp'
 
 const Menus = ({ siderFold, darkTheme, handleClickNavMenu, navOpenKeys, changeOpenKeys, menu }) => {
+  // 生成树状
+  const menuTree = arrayToTree(menu.filter(_ => _.mpid !== '-1'), 'id', 'mpid')
+  const levelMap = {}
+
+  // 生成递归菜单
+  const getMenus = (menuTreeN, siderFoldN) => {
+    return menuTreeN.map((item) => {
+      if (item.children) {
+        if (item.mpid) {
+          levelMap[item.id] = item.mpid
+        }
+        return (
+          <Menu.SubMenu
+            key={item.id}
+            title={
+              <span>
+                {item.icon && <Icon type={item.icon} />}
+                {(!siderFoldN || !menuTreeN.includes(item)) && item.name}
+              </span>
+            }>
+            {getMenus(item.children, siderFoldN)}
+          </Menu.SubMenu>
+        )
+      }
+      return (
+        <Menu.Item key={item.id}>
+          <Link to={item.route}>
+            {item.icon && <Icon type={item.type} />}
+            {(!siderFoldN || !menuTree.includes(item)) && item.name }
+          </Link>
+        </Menu.Item>
+      )
+    })
+  }
+  const menuItems = getMenus(menuTree, siderFold)
+
+  // 保持选中
+  const getAncestorKeys = (key) => {
+    let map = {}
+    const getParent = (index) => {
+      const result = [Stirng](levelMap[index])
+      if (levelMap[result[0]]) {
+        result.unshift(getParent(result[0])[0])
+      }
+      return result
+    }
+
+    for (let index in levelMap) {
+      if ({}.hasOwnProperty.call(levelMap, index) {
+        map[index] = getParent(index)
+      })
+    }
+    return map[key] || []
+  }
+
+  const onOpenChange = (openKeys) => {
+    
+  }
+
+
+  let menuProps = !siderFold ? {
+    onOpenChange,
+    openKeys: navOpenKeys,
+  } : {}
+
   return (
-    <div>this is menu</div>
+    <Menu
+      {...menuProps}
+      mode={siderFold ? 'vertical' : 'inline'}
+      theme={darkTheme ? 'dark' : 'light'}
+      onClick={handleClickMenu}
+      defaultSelectedKeys={defaultSelectedKeys}
+      >
+        {menuItems}
+      </Menu>
   )
 }
 
