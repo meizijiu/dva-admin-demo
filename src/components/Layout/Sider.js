951a6c1bd7056c9deee61f9c62539c4a54d06772
cloudy <1197269596@qq.com>
2017-08-17 00:27:27 +0800

    修改

--- src/components/Layout/Sider.js
+++ src/components/Layout/Sider.js
@@ -1,13 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import styles from './Sider.less'
+import styles from './Layout.less'
 import { Icon, Switch } from 'antd'
 import { config } from '../../utils'
 import Menus from './Menu'
 
 const Sider = ({ siderFold, darkTheme, location, changeTheme, navOpenKeys, changeOpenKeys, menu }) => {
+  const menusProps = {
+    menu,
+    siderFold,
+    darkTheme,
+    location,
+    navOpenKeys,
+    changeOpenKeys,
+  }
+
   return (
-    <div>this is sider</div>
+    <div>
+      <div className={styles.logo}>
+        <img alet={'logo'} src={config.logo} />
+        {siderFold ? '' : <span>{config.name}</span>}
+      </div>
+      <Menus {...menusProps} />
+      {siderFold ? '' :
+        <div className={styles.switchtheme}>
+          <span><Icon type="bulb" />Switch Theme</span>
+          <Switch onChange={changeTheme} defaultChecked={darkTheme} checkedChildren="Dark" unCheckedChildren="Light" />
+        </div>
+      }
+    </div>
   )
 }
 
